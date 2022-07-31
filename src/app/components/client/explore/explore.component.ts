f1bb6a7054998397a6adf9b7d8e01ef2f5ba4f0c
Osmar1n <oortiz-2020104@kinal.edu.gt>
2022-07-31 14:13:04 -0600

    Añadido agregar destinos en explorar

--- src/app/components/client/explore/explore.component.ts
+++ src/app/components/client/explore/explore.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { CategoryRestService } from 'src/app/services/category/category-rest.service';
 import { DepartmentRestService } from 'src/app/services/department/department-rest.service';
+import { DestinyRestService } from 'src/app/services/destiny/destiny-rest.service';
+import { TripRestService } from 'src/app/services/trip/trip-rest.service';
 import { TuristicCenterRestService } from 'src/app/services/turisticCenter/turistic-center-rest.service';
 import { UserRestService } from 'src/app/services/user/user-rest.service';
 import { environment } from 'src/environments/environment';
+
+import { DestinyModel } from 'src/app/models/destiny.model';
 
 import Swal from 'sweetalert2';
 
@@ -18,15 +23,28 @@ export class ExploreComponent implements OnInit {
     private userRest: UserRestService,
     private departmentRest: DepartmentRestService,
     private categoryRest: CategoryRestService,
-    private turisticCenterRest: TuristicCenterRestService
-  ) {}
+    private turisticCenterRest: TuristicCenterRestService,
+    private tripRest: TripRestService,
+    private destinyRest: DestinyRestService,
+    private router: Router
+  ) {
+    this.destiny = new DestinyModel('', '', '', new Date(), new Date());
+  }
 
   ngOnInit(): void {
-    this.getDepartmentsOnlyContributor()
-    this.getCategoriesOnlyContributor()
-    this.getTuristicsCentersOnlyClient()
+    this.getDepartmentsOnlyContributor();
+    this.getCategoriesOnlyContributor();
+    this.getTuristicsCentersOnlyClient();
+
+    this.today = new Date()
+      .toISOString()
+      .slice(0, new Date().toISOString().lastIndexOf(':'));
   }
-  
+
+  today: any;
+
+  //* Centros turísticos ---------------------------------------------------------------------------------------
+
   uri: string = environment.baseUrl + 'turisticCenter/getImageTuristicCenter/';
 
   searchTuristicCenterName: String = '';
@@ -37,6 +55,7 @@ export class ExploreComponent implements OnInit {
   categories: any;
 
   turisticsCenters: any;
+  turisticCenterGetId: any;
 
   getDepartmentsOnlyContributor() {
     this.departmentRest.getDepartmentsOnlyClient().subscribe({
@@ -69,5 +88,61 @@ export class ExploreComponent implements OnInit {
         console.log(err.error.message);
       },
     });
+  }
+
+  //* Viajes ---------------------------------------------------------------------------------------
+  tripsLenght: any;
+
+  trips: any;
+  tripGetData: any;
+  tripGetId: any;
+
+  getTrips(turisticCenterId: string) {
+    this.turisticCenterGetId = turisticCenterId;
+    this.tripRest.getTrips().subscribe({
+      next: (res: any) => {
+        this.trips = res.trips;
+        this.tripsLenght = this.trips.length;
+      },
+      error: (err: any) => {
+        console.log(err.error.message);
+      },
+    });
+  }
+
+  //* Destinos ---------------------------------------------------------------------------------------
+  destiny: DestinyModel;
+
+  getTripId(idTrip: string) {
+    this.tripGetId = idTrip;
+  }
+
+  addDestiny() {
+    this.destinyRest
+      .addDestiny(this.destiny, this.tripGetId, this.turisticCenterGetId)
+      .subscribe({
+        next: (res: any) => {
+          Swal.fire({
+            title: 'Destino añadido al viaje',
+            text: '¿Deseas ir a tus viajes?',
+            icon: 'success',
+            showCancelButton: true,
+            confirmButtonColor: '#198754',
+            cancelButtonColor: '#aaa',
+            confirmButtonText: 'Ir a mis viajes',
+            cancelButtonText: 'Seguir añadiendo destinos',
+          }).then((result) => {
+            if (result.isConfirmed) {
+              this.router.navigateByUrl('/myTrips');
+            }
+          });
+        },
+        error: (err: any) => {
+          Swal.fire({
+            icon: 'warning',
+            title: err.error.message || err.error,
+          });
+        },
+      });
   }
 }
---
+++ src/app/models/destiny.model.ts
@@ -0,0 +1,9 @@
+export class DestinyModel {
+  constructor(
+    public id: string,
+    public trip: string,
+    public turisticCenter: string,
+    public startDate: Date,
+    public endDate: Date
+  ) {}
+}
